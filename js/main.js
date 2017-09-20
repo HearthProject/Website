eea1ec8d3bc7a829f74bb908a68b6d7ae8671dfc
Stuart <Rushmead@techie.com>
2017-09-20 09:18:22 +0100

    Optimisations

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(".button-collapse").sideNav({
+    edge: 'right',
+    closeOnClick: true 
+  });
+  var app = angular.module('hearthProject', ['ngRoute']);
+  app.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'views/index.html'
+    }).when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginController'
+    }).when('/client', {
+      templateUrl: 'views/client.html'
+    }).when('/download', {
+      templateUrl: 'views/alpha.html',
+      controller: 'DownloadsController'
+    });
+  });
+  app.controller('LoginController', function ($scope) {
+  });
+  app.controller('PacksController', function ($scope) {
+  });
+  app.controller('DownloadsController', function ($scope, $http) {
+      $scope.versions = [];
+      $http({
+          method: 'GET',
+          url: '/data/download.json'
+        }).then(function successCallback(response) {
+              console.log(response.data['downloads']);  
+              var data = response.data;
+              for(var version in data['downloads']){
+                  console.log(data['downloads'][version]);
+                  $scope.versions.push(data['downloads'][version]);
+              }
+              console.log($scope.versions);
+          }, function errorCallback(response) {
+              // called asynchronously if an error occurs
+              // or server returns response with an error status.
+              throw response;
+          });
+  
+  });
+  
