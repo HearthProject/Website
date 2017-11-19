06fcc0df874937c241ac213263b5942f18856f0d
Stuart <Rushmead@techie.com>
2017-11-19 19:47:23 +0000

    :sparkles: Hearth Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
-(function($){$(function(){$(".button-collapse").sideNav({edge:'right',closeOnClick:!0})})})(jQuery);var app=angular.module('hearthProject',['ngRoute']);app.config(function($routeProvider){$routeProvider.when('/',{templateUrl:'views/index.html'}).when('/login',{templateUrl:'views/login.html',controller:'LoginController'}).when('/client',{templateUrl:'views/client.html'}).when('/download',{templateUrl:'views/alpha.html',controller:'DownloadsController'})});app.controller('LoginController',function($scope){});app.controller('PacksController',function($scope){});app.controller('DownloadsController',function($scope,$http){$scope.versions=[];$http({method:'GET',url:'/data/download.json'}).then(function successCallback(response){console.log(response.data.downloads);var data=response.data;for(var version in data.downloads){console.log(data.downloads[version]);$scope.versions.push(data.downloads[version])}console.log($scope.versions)},function errorCallback(response){throw response})})
+(function ($) {
+    $(function () {
+        $(".button-collapse").sideNav({
+            edge: 'right',
+            closeOnClick: !0
+        })
+    })
+})(jQuery);
+var app = angular.module('hearthProject', ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'views/index.html'
+    }).when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginController'
+    }).when('/launcher', {
+        templateUrl: 'views/launcher.html'
+    }).when('/download', {
+        templateUrl: 'views/alpha.html',
+        controller: 'DownloadsController'
+    })
+});
+app.controller('LoginController', function ($scope) {});
+app.controller('PacksController', function ($scope) {});
+app.controller('DownloadsController', function ($scope, $http) {
+    $scope.windows = {};
+    $scope.linux = {};
+    $http({
+        method: 'GET',
+        url: '/data/win32.json'
+    }).then(function successCallback(response) {
+        var data = response.data;
+        var versionNumber = Object.keys(data.versions)[0];
+        var version = data.versions[Object.keys(data.versions)[0]];
+        version.version = versionNumber;
+        $scope.windows = version;
+    }, function errorCallback(response) {
+        throw response
+    });
+    $http({
+        method: 'GET',
+        url: '/data/linux.json'
+    }).then(function successCallback(response) {
+        console.log(response.data.downloads);
+        var data = response.data;
+        var versionNumber = Object.keys(data.versions)[0];
+        var version = data.versions[Object.keys(data.versions)[0]];
+        version.version = versionNumber;
+        $scope.linux = version;
+    }, function errorCallback(response) {
+        throw response
+    });
+})
